60e3c8cebfe76ffa72f0159c0f0c00bb5d44a83c
21-Shivansh <shivanshsingh2111@gmail.com>
2026-05-23 00:24:34 +0530

    js

---
+++ basic.js
@@ -0,0 +1,14 @@
+// let maths = Number(prompt('Enter maths marks'))
+// let engl = Number(prompt('Enter eng marks'))
+// let sci = Number(prompt('Enter sci marks'))
+
+// let avg = (maths + engl + sci) /3;
+
+// if(avg >= 8){
+//     console.log(avg)
+//     console.log("You are a good student")   
+// }else{
+//     console.log(avg)
+//     console.log("need imgprovemnt")
+// }
+
