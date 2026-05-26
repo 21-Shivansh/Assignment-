8d271139181ab692e67e7441ce34579636d14260
21-Shivansh <shivanshsingh2111@gmail.com>
2026-05-26 22:58:00 +0530

    somefunction

--- basic.js
+++ basic.js
@@ -12,3 +12,46 @@
 //     console.log("need imgprovemnt")
 // }
 
+// let a = 10;
+
+// while( a > 1){
+//     if(a % 2 != 0){
+//         console.log(false);
+//         break;
+//     }else{
+//         a /= 2;
+//     }
+// }
+
+// if(a === 1){
+//     console.log(true)
+// }
+
+// let a = 15;
+
+// let b = Math.sqrt(a);
+
+// if (a%b === 0) {
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// let a = 12321;
+
+// let org = a;
+// let rev = 0;
+
+// while(a > 0){
+//     let digit = a % 10;
+//     rev = rev*10 + digit;
+//     a = Math.floor(a/10);
+// }
+
+// if(org == rev){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
