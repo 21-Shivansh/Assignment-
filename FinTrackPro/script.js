a497e83d2a56731d86d1b332a883fd9c9cfe2330
21-Shivansh <shivanshsingh2111@gmail.com>
2026-07-07 00:35:41 +0530

    ftpupdate5

--- FinTrackPro/script.js
+++ FinTrackPro/script.js
@@ -1,6 +1,21 @@
 let addTrans = document.querySelector('.dn_button');
 let formDiv = document.querySelector('.form_div');
 let fcross = document.querySelector('.fh_cross');
+let dashpg = document.querySelector('.dashboardPage');
+let setpg = document.querySelector('.settingPage');
+let  dashtab = document.querySelector('.sb_optDiv1');
+let settab = document.querySelector('.sb_optDiv2');
+
+dashtab.addEventListener('click',()=>{
+    setpg.style.display = 'none';
+    dashpg.style.display = 'flex';
+
+})
+
+settab.addEventListener('click',()=>{
+    dashpg.style.display = 'none';
+    setpg.style.display = 'flex';
+})
 
 
 addTrans.addEventListener('click',()=>{
