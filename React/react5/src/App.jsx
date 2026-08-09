b4abe05723c2ffecb9b111586ba0aa9e85bc6211
21-Shivansh <shivanshsingh2111@gmail.com>
2026-08-09 20:35:36 +0530

    Understanding RHF

--- React/react5/src/App.jsx
+++ React/react5/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Cart from './components/Cart'
+import RHF from './components/RHF'
 
 const App = () => {
   return (
     <div className='h-screen bg-gray-900'>
-      <Cart />
+      {/* <Cart /> */}
+      <RHF />
     </div>
   )
 }
