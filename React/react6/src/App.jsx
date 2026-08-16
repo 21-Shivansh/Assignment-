85fc11cccce221a90666c0f57aaf2fdb36b50047
21-Shivansh <shivanshsingh2111@gmail.com>
2026-08-16 18:42:12 +0530

    understanding crud in RHF

---
+++ React/react6/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Navbar from './component/Navbar'
+import PopForm from './component/PopForm'
+import AllCars from './pages/AllCars'
+
+const App = () => {
+  const [toggle,setToggle] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [edtUser,setEdtUser] = useState(null);
+  return (
+    <div className='min-h-screen bg-gray-800 text-white p-2 flex flex-col'>
+      <Navbar setToggle={setToggle} setEdtUser={setEdtUser} />
+      {
+        toggle ? (<PopForm setUsers={setUsers} users={users} edtUser={edtUser} setEdtUser={setEdtUser} setToggle={setToggle} />) : (<AllCars setEdtUser={setEdtUser} setUsers={setUsers} setToggle={setToggle} users={users} />) 
+      }
+    </div>
+  )
+}
+
+export default App
